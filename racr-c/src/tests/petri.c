4cf205c7a0c657852d005d0543baa54debf49445
twobit <twobit@lavabit.com>
2014-02-08 23:54:59 +0100

    implement all transitions

--- racr-c/src/tests/petri.c
+++ racr-c/src/tests/petri.c
@@ -7,25 +7,42 @@
 Scheme_Env* global_env;
 
 
-
-Scheme_Object* fire_callback(int argc, Scheme_Object** argv) {
-	printf("CALLBACK - transition name: %s\n",
-		SCHEME_SYM_VAL(racr_ast_child_by_name("name", argv[0])));
-
-	return scheme_false;
-}
-
-
-
 Scheme_Object* eq_Token(int argc, Scheme_Object** argv) {
 	return racr_call(scheme_eval_string("eq?", global_env), "*s", argv[0], "Token");
 }
 Scheme_Object* eq_Euro(int argc, Scheme_Object** argv) {
 	return racr_call(scheme_eval_string("eq?", global_env), "*s", argv[0], "Euro");
 }
+Scheme_Object* ge_two(int argc, Scheme_Object** argv) {
+	return racr_call(scheme_eval_string(">=", global_env), "*i", argv[0], 2);
+}
+Scheme_Object* take_any(int argc, Scheme_Object** argv) {
+	return scheme_true;
+}
+
 
 Scheme_Object* yield_Euro(int argc, Scheme_Object** argv) {
 	return racr_list("s", "Euro");
+}
+Scheme_Object* yield_Token(int argc, Scheme_Object** argv) {
+	return racr_list("s", "Token");
+}
+Scheme_Object* yield_minus_two(int argc, Scheme_Object** argv) {
+	long x;
+	scheme_get_int_val(argv[1], &x);
+	return racr_list("i", x - 2);
+}
+Scheme_Object* yield_yz(int argc, Scheme_Object** argv) {
+	return racr_list("**", argv[1], argv[2]);
+}
+
+
+Scheme_Object* fire_callback(int argc, Scheme_Object** argv) {
+	printf("CALLBACK - transition name: %s\n",
+		SCHEME_SYM_VAL(racr_ast_child_by_name("name", argv[0])));
+
+
+	return scheme_false;
 }
 
 
@@ -48,6 +65,20 @@ int main(int argc, char** argv) {
 
 	racr_call(scheme_eval_string("init-ast", env), "");
 	racr_call(scheme_eval_string("init-attribution", env), "");
+
+
+
+	Scheme_Object* wrap_eq_Euro = scheme_make_prim_w_arity(eq_Euro, "eq_Euro", 1, 1);
+	Scheme_Object* wrap_eq_Token = scheme_make_prim_w_arity(eq_Token, "eq_Token", 1, 1);
+	Scheme_Object* wrap_ge_two = scheme_make_prim_w_arity(ge_two, "ge_two", 1, 1);
+	Scheme_Object* wrap_take_any = scheme_make_prim_w_arity(take_any, "take_any", 1, 1);
+
+	Scheme_Object* wrap_yield_Euro = scheme_make_prim_w_arity(yield_Euro, "yield_Euro", 1, -1);
+	Scheme_Object* wrap_yield_Token = scheme_make_prim_w_arity(yield_Token, "yield_Token", 1, -1);
+	Scheme_Object* wrap_yield_minus_two = scheme_make_prim_w_arity(yield_minus_two, "yield_minus_two", 1, -1);
+	Scheme_Object* wrap_yield_yz = scheme_make_prim_w_arity(yield_yz, "yield_yz", 1, -1);
+
+	Scheme_Object* wrap_fire_callback = scheme_make_prim_w_arity(fire_callback, "fire_callback", 1, 1);
 
 
 
@@ -88,14 +119,49 @@ int main(int argc, char** argv) {
 
 
 			// transitions
-			racr_create_ast_list(racr_list("*",
+			racr_create_ast_list(racr_list("*****",
 				racr_create_ast(petri_spec, "EventTransition", racr_list("s***",
 					"c",
-					racr_create_ast_list(racr_list("*", racr_create_ast(petri_spec, "Arc", racr_list("s*",
-						"D", racr_list("*", scheme_make_prim_w_arity(eq_Token, "eq_Token", 1, 1)))))),
-					racr_create_ast_list(racr_list("*", racr_create_ast(petri_spec, "Arc", racr_list("s*",
-						"A", scheme_make_prim_w_arity(yield_Euro, "yield_Euro", 1, -1))))),
-					scheme_make_prim_w_arity(fire_callback, "callback", 1, 1))))),
+					racr_create_ast_list(racr_list("*",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "D", racr_list("*", wrap_eq_Token))))),
+					racr_create_ast_list(racr_list("*",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "A", wrap_yield_Euro)))),
+					wrap_fire_callback)),
+				racr_create_ast(petri_spec, "EventTransition", racr_list("s***",
+					"e",
+					racr_create_ast_list(racr_list("*",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "A", racr_list("*", wrap_eq_Euro))))),
+					racr_create_ast_list(racr_list("*",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "D", wrap_yield_Token)))),
+					wrap_fire_callback)),
+
+				racr_create_ast(petri_spec, "EventTransition", racr_list("s***",
+					"a",
+					racr_create_ast_list(racr_list("***",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "A", racr_list("*", wrap_eq_Euro))),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "E", racr_list("*", wrap_ge_two))),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "G", racr_list("*", wrap_eq_Token))))),
+					racr_create_ast_list(racr_list("****",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "D", wrap_yield_Token)),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "E", wrap_yield_minus_two)),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "F", wrap_yield_Euro)),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "B", wrap_yield_Token)))),
+					wrap_fire_callback)),
+				racr_create_ast(petri_spec, "EventTransition", racr_list("s***",
+					"b",
+					racr_create_ast_list(racr_list("**",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "B", racr_list("*", wrap_eq_Token))),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "H", racr_list("**", wrap_take_any, wrap_take_any))))),
+					racr_create_ast_list(racr_list("**",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "G", wrap_yield_Token)),
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "C", wrap_yield_yz)))),
+					wrap_fire_callback)),
+				racr_create_ast(petri_spec, "EventTransition", racr_list("s***",
+					"d",
+					racr_create_ast_list(racr_list("*",
+						racr_create_ast(petri_spec, "Arc", racr_list("s*", "C", racr_list("*", wrap_take_any))))),
+					racr_create_ast_list(racr_list("")),
+					wrap_fire_callback)))),
 
 
 			// ports
@@ -107,23 +173,31 @@ int main(int argc, char** argv) {
 	Scheme_Object* print_marking = scheme_eval_string("print-marking", env);
 	Scheme_Object* print_enabled = scheme_eval_string("print-enabled", env);
 
-	racr_call(print_marking, "*", pn);
-	racr_call(print_enabled, "*", pn);
 
+	char line[256];
+	for (;;) {
+		racr_call(print_marking, "*", pn);
+		racr_call(print_enabled, "*", pn);
 
-	printf("\n");
-	printf("Firing transition c.\n");
-	Scheme_Object* trans = racr_att_value("find-transition", pn, racr_list("s", "c"));
-	racr_call(fire_transition, "*", trans);
-	if (racr_is_ast_subtype_nt(trans, "EventTransition")) {
-		racr_call(racr_ast_child_by_name("callback", trans), "*", trans);
+		printf("Enter transition to fire:\n> ");
+		fgets(line, 256, stdin);
+		int l = strlen(line);
+		if (l == 0) break;
+		line[l - 1] = '\0';
+
+		Scheme_Object* trans = racr_att_value("find-transition", pn, racr_list("s", line));
+		if (trans == scheme_false) {
+			printf("Error optaining transition\n");
+			break;
+		}
+
+		printf("Firing transition...\n");
+		racr_call(fire_transition, "*", trans);
+		if (racr_is_ast_subtype_nt(trans, "EventTransition")) {
+			racr_call(racr_ast_child_by_name("callback", trans), "*", trans);
+		}
+		printf("\n");
 	}
-	printf("Transition fired.\n");
-	printf("\n");
-
-
-	racr_call(print_marking, "*", pn);
-	racr_call(print_enabled, "*", pn);
 
 
 
