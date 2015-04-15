03d50a9109d71cd2e9fe1ba2db3391ea93219afd
cbuerger <christoff.buerger@gmx.net>
2015-04-15 23:46:04 +0200

    Examples (atomic-petrinets): Added profiling scenario.

---
+++ profiling/atomic-petrinets/profiling.scm
@@ -0,0 +1,46 @@
+; This program and the accompanying materials are made available under the
+; terms of the MIT license (X11 license) which accompanies this distribution.
+
+; Author: C. Bürger
+
+#!r6rs
+
+(library
+ (atomic-petrinets profiling)
+ (export make-profiling-net)
+ (import (rnrs) (racr core) (atomic-petrinets query-support)
+         (atomic-petrinets user-interface)
+         )
+ 
+ (define (make-profiling-net $transitions $influenced $tokens $local-places)
+   (define (make-name . i)
+     (string->symbol (apply string-append "n" (map number->string (map - i)))))
+   (define places (list))
+   (define transitions (list))
+   
+   ;;; Construct segments:
+   (do ((index 0 (+ index 1))) ((= index $transitions))
+     (let ((out (list))
+           (in (list (:Arc (make-name index) (list (lambda (t) t))))))
+       (do ((offset 0 (+ offset 1))) ((> offset $influenced))
+         (let ((name (make-name (mod (+ index offset 1) $transitions))))
+           (set! places (cons (:Place (make-name index)) places))
+           (set! out (cons (:Arc name (lambda x #t)) out))))
+       ; Connect local places:
+       (do ((offset 0 (+ offset 1))) ((> offset $local-places))
+         (let ((name (make-name index offset)))
+           (set! places (cons (:Place name) places))
+           (set! in (cons (:Arc name (list (lambda (t) t))) in))
+           (set! out (cons (:Arc name (lambda x #t)) out))))
+       ; Initialise the transition:
+       (set! transitions (cons (:Transition (make-name index) in out) transitions))))
+   
+   ;;; Construct Petri net:
+   (let ((net (:AtomicPetrinet places transitions)))
+     (unless (=valid? net)
+       (exception: "Cannot construct Petri net; The net is not well-formed."))
+     net))
+ 
+ (initialise-petrinet-language)
+ ;(run-petrinet! (make-profiling-net 56 20 13 3))
+ )
