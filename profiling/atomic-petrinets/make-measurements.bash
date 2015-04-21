45472f2386db5d56281b12a0d982f31597af0bce
cbuerger <christoff.buerger@gmx.net>
2015-04-21 16:06:47 +0200

    Examples (atomic-petrinets): Benchmarking infrastructure (WORK IN PROGRESS); Finished parameter configuration and iteration.

--- profiling/atomic-petrinets/make-measurements.bash
+++ profiling/atomic-petrinets/make-measurements.bash
@@ -24,28 +24,30 @@ declare -a parameter_names
 declare -a parameter_values
 declare -a parameter_iterations
 declare -a parameter_adjustments
+
+echo "************************************************** Configure Parameters **************************************************"
 while read -u3 line
 do
 	IFS='/' read -ra config_line <<< "$line"
 	
 	parameter_names+=( "${config_line[0]}" )
 	
-	read -r -p "${config_line[1]}: " choice
-	if [ ! -t 0 ]; then echo "${config_line[1]}: $choice"; fi
+	read -r -p "${config_line[1]} [${config_line[0]}]: " choice
+	if [ ! -t 0 ]; then echo "${config_line[1]} [${config_line[0]}]: $choice"; fi
 	echo $choice >> $old_pwd/rerun-measurements.bash
 	parameter_values+=( "$choice" )
 	
-	read -r -n1 -p " Iterate? (y/n): " choice
-	if [ ! -t 0 ]; then printf " Iterate? (y/n): $choice"; fi
+	read -r -n1 -p "	Iterate? (y/n): " choice
+	if [ ! -t 0 ]; then printf "	Iterate? (y/n): $choice"; fi
 	echo ""
 	printf $choice >> $old_pwd/rerun-measurements.bash
 	case $choice in
-		[y]* )	read -r -p " Number of iterations: " choice
-				if [ ! -t 0 ]; then echo " Number of iterations: $choice"; fi
+		[y]* )	read -r -p "	Number of iterations: " choice
+				if [ ! -t 0 ]; then echo "	Number of iterations: $choice"; fi
 				echo $choice >> $old_pwd/rerun-measurements.bash
 				parameter_iterations+=( "$choice" )
-				read -r -p " Adjustment each iteration: " choice
-				if [ ! -t 0 ]; then echo " Adjustment each iteration: $choice"; fi
+				read -r -p "	Adjustment each iteration: " choice
+				if [ ! -t 0 ]; then echo "	Adjustment each iteration: $choice"; fi
 				echo $choice >> $old_pwd/rerun-measurements.bash
 				parameter_adjustments+=( "$choice" );;
 		[n]* )	parameter_iterations+=( 1 )
@@ -65,50 +67,46 @@ cp -p rerun-measurements.bash $measurement_dir
 
 ########################################################################################################### Perform measurements:
 num_parameters=${#parameter_names[@]}
-current_values=parameter_values
-current_iterations=( )
 current_parameter=0
+current_values[0]=$(( parameter_values[0] - parameter_adjustments[0] ))
 run=true
-while $run
+undo=false
+
+echo "*************************************************** Start Measurements ***************************************************"
+while [ "$run" = true ]
 do
-	if (( current_parameter == num_parameters ))
+	if (( current_parameter >= num_parameters ))
 	then
-		echo ${current_values[@]}
-		if (( current_iterations[0] == parameter_iterations[0] ))
+		#echo ${current_values[@]:0:$num_parameters}
+		printf "Measurement ["
+		undo=true
+		run=false
+		for (( i = 0; i < num_parameters; i++ ))
+		do
+			printf " ${parameter_names[$i]}=${current_values[$i]} "
+			if (( current_iterations[i] < parameter_iterations[i] ))
+			then
+				run=true
+			fi
+		done
+		echo "]"
+	fi
+	if [ "$undo" = true ]
+	then
+		current_parameter=$current_parameter-1
+		if (( current_iterations[current_parameter] < parameter_iterations[current_parameter] ))
 		then
-			run=false
 			undo=false
 		fi
 	else
-		if undo
-		then
-			current_parameter=$current_parameter-1
-		else
-			current_values[$current_parameter]=$(( current_values[current_parameter] + parameter_adjustments[current_parameter] ))
-			current_iterations[$current_parameter]=$(( current_iterations[current_parameter] + 1 ))
-			current_parameter=$current_parameter+1
-			current_values[$current_parameter]=$(( parameter_values[current_parameter] ))
-			current_iterations[$current_parameter]=0
-		fi
+		current_values[$current_parameter]=$(( current_values[current_parameter] + parameter_adjustments[current_parameter] ))
+		current_iterations[$current_parameter]=$(( current_iterations[current_parameter] + 1 ))
+		current_parameter=$(( current_parameter + 1 ))
+		current_values[$current_parameter]=$(( parameter_values[current_parameter] - parameter_adjustments[current_parameter]))
+		current_iterations[$current_parameter]=0
 	fi
 done
-
-:'measure(){
-	if (( current_parameter == num_parameters ))
-	then
-		echo ${current_values[@]}
-	else
-		for (( i = 1; i <= parameter_iterations[current_parameter]; i++ ))
-		do
-			current_values[$current_parameter]=$(( current_values[current_parameter] + parameter_adjustments[current_parameter] ))
-			#current_values[$current_parameter+1]=$(( parameter_values[current_parameter + 1] ))
-			current_parameter=$current_parameter+1
-			measure
-			current_iteration[$current_parameter]=$(( current_iteration[current_parameter] + 1 ))
-		done
-	fi
-}
-measure'
+echo "**************************************************** End Measurements ****************************************************"
 
 ################################################################################# Finish execution & cleanup temporary resources:
 my_exit
