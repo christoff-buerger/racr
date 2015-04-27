c87557034f5bd7aef548e735a6fb5b3823f08af5
twobit <s8572327@gmail.com>
2015-04-27 16:17:12 +0200

    stuff

--- racr-net/Solutions/Questionnaire/Questionnaire/Questionnaire.cs
+++ racr-net/Solutions/Questionnaire/Questionnaire/Questionnaire.cs
@@ -1,17 +1,8 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Collections.Generic;
 using System.Windows.Forms;
-
-
-// for testing
-using IronScheme;
-using IronScheme.Runtime;
-using IronScheme.Scripting;
-
 
 
 enum ValueTypes { Boolean, String, Number, ErrorType }
@@ -99,8 +90,16 @@ class CheckWidget : Widget {
 
 
 class QL : Racr.Specification {
+	static QL ql;
 
-	public QL() {
+	public static QL Ql {
+		get {
+			if (ql == null) ql = new QL();
+			return ql;
+		}
+	}
+
+	QL() {
 		AstRule("Form->Element*<Body");
 		AstRule("Element->");
 		AstRule("Group:Element->Expression-Element*<Body");
@@ -115,9 +114,6 @@ class QL : Racr.Specification {
 		RegisterAgRules();
 		CompileAgSpecifications();
 	}
-
-
-
 
 	static Racr.AstNode findL(string name, Racr.AstNode l, int i) {
 		for (int j = 1; j <= i; j++) {
@@ -149,6 +145,30 @@ class QL : Racr.Specification {
 		form.Text = "Questionnaire";
 
 		// TODO: menu
+		var file = new MenuItem("&File");
+		var open = new MenuItem("&Open");
+		var save = new MenuItem("&Save");
+		var quit = new MenuItem("&Quit");
+		open.Click += (object sender, EventArgs e) => {
+			var ofd = new OpenFileDialog();
+			if (ofd.ShowDialog() == DialogResult.OK) {
+				var parser = new Parser(Ql, File.OpenText(ofd.FileName).ReadToEnd());
+				var ast = parser.ParseAst();
+				ast.Render();
+				Questionnaire.UpdateQuestions(ast);
+
+				form.Close();
+
+			}
+		};
+		quit.Click += (object sender, EventArgs e) => { form.Close(); };
+
+		file.MenuItems.Add(open);
+		file.MenuItems.Add(save);
+		file.MenuItems.Add(quit);
+		form.Menu = new MainMenu();
+		form.Menu.MenuItems.Add(file);
+
 
 		var panel = new FlowLayoutPanel();
 		panel.AutoSize = true;
@@ -159,10 +179,13 @@ class QL : Racr.Specification {
 
 		form.Controls.Add(panel);
 
+		form.Show();
+
 		return panel;
 	}
 	[Racr.AgRule("Render", "Form")]
 	static bool FormRender(Racr.AstNode n) {
+		n.Widget();
 		foreach (var c in n.GetBody().Children()) {
 			var child = c as Racr.AstNode;
 			var w = child.Widget();
@@ -408,213 +431,10 @@ class QL : Racr.Specification {
 }
 
 
-class Lexer {
-
-	public enum Lexemes {
-		EOF,
-		LeftParenthesis,
-		RightParenthesis,
-		String,
-		Symbol,
-		Number,
-		Identifier,
-		Error
-	};
-	
-	public Lexer(string src) {
-		source = src;
-		position = 0;
-		NextChar();
-		NextLexeme();
-	}
-
-	public Lexemes NextLexeme() {
-		Lexemes l = lexeme;
-		lexeme = Scan();
-		return l;
-	}
-
-	private char NextChar() {
-		char c = character;
-		if (position >= source.Length) character = '\0';
-		else character = source[position++];
-		return c;
-	}
-
-	private Lexemes Scan() {
-		for (;;) {
-			if (Char.IsWhiteSpace(character)) {
-				NextChar();
-				continue;
-			}
-			if (character == ';') {
-				do NextChar();
-				while (character != '\0' && character != '\n');
-				continue;
-			}
-			break;
-		}
-
-		token = "";
-
-		switch (character) {
-		case '(': NextChar(); return Lexemes.LeftParenthesis;
-		case ')': NextChar(); return Lexemes.RightParenthesis;
-		case '\0': return Lexemes.EOF;
-		case '\'':
-			NextChar();
-			while (Char.IsLetterOrDigit(character)) token += NextChar();
-			return Lexemes.Symbol;
-		case '"':
-			NextChar();
-			while (character != '"') {
-				if (character == '\0') return Lexemes.Error;
-				else if (character == '\\') {
-					NextChar();
-					if (character == '\\') token += '\\';
-					else if (character == '"') token += '"';
-					else if (character == 'n') token += '\n';
-					else return Lexemes.Error;
-				}
-				else token += NextChar();
-			}
-			NextChar();
-			return Lexemes.String;
-		default: break;
-		}
-
-		if (Char.IsDigit(character)) {
-			while (Char.IsDigit(character)) token += NextChar();
-			if (character == '.') {
-				token += NextChar();
-				while (Char.IsDigit(character)) token += NextChar();
-			}
-			return Lexemes.Number;
-		}
-
-		while (Char.IsLetterOrDigit(character) || "+-*/:&=<>?!~$#".Contains(character)) {
-			token += NextChar();
-		}
-		if (token.Length > 0) return Lexemes.Identifier;
-
-		return Lexemes.Error;
-	}
-
-	public Lexemes Lexeme { get { return lexeme; } }
-	public string Token { get { return token; } }
-
-	private char character;
-	private Lexemes lexeme;
-	private string token;
-
-	private string source;
-	private int position;
-}
-
-
-class Parser : Lexer {
-	private Racr.Specification spec;
-
-	public Parser(Racr.Specification spec, string src) : base(src) {
-		this.spec = spec;
-	}
-
-	private void Consume(Lexemes lexeme) {
-		if (Lexeme != lexeme) throw new Exception("Parse Exception");
-		NextLexeme();
-	}
-	private string ParseIdentifier() {
-		var t = Token;
-		Consume(Lexemes.Identifier);
-		return t;
-	}
-	private string ParseString() {
-		var t = Token;
-		Consume(Lexemes.String);
-		return t;
-	}
-	private string ParseSymbol() {
-		var t = Token;
-		Consume(Lexemes.Symbol);
-		return t;
-	}
-	private object ParseValue() {
-		var t = Token;
-		switch (NextLexeme()) {
-		case Lexemes.Identifier:
-			if (t == "#t") return true;
-			if (t == "#f") return false;
-			throw new Exception("Parse Error");
-		case Lexemes.Number: return Convert.ToDouble(t);
-		case Lexemes.String: return t;
-		default: throw new Exception("Parse Error");
-		}
-	}
-
-	private Racr.AstNode ParseExpression() {
-		Racr.AstNode c;
-		List<Racr.AstNode> e, a;
-		string n, l, o;
-		ValueTypes t;
-		object v;
-
-		Consume(Lexemes.LeftParenthesis);
-		switch (ParseIdentifier()) {
-		case "Form":
-			e = new List<Racr.AstNode>();
-			e.Add(new Racr.AstNode(spec, "ComputedQuestion", "ErrorType", "", new Racr.AstNode(spec, "Constant", false)));
-			while (Lexeme == Lexemes.LeftParenthesis) e.Add(ParseExpression());
-			Consume(Lexemes.RightParenthesis);
-			return new Racr.AstNode(spec, "Form", new Racr.AstList(e.ToArray()));
-		case "If":
-			c = ParseExpression();
-			e = new List<Racr.AstNode>();
-			while (Lexeme == Lexemes.LeftParenthesis) e.Add(ParseExpression());
-			Consume(Lexemes.RightParenthesis);
-			return new Racr.AstNode(spec, "Group", c, new Racr.AstList(e.ToArray()));
-		case "??":
-			n = ParseSymbol();
-			l = ParseString();
-			t = (ValueTypes) Enum.Parse(typeof(ValueTypes), ParseIdentifier());
-			if (Lexeme == Lexemes.RightParenthesis) v = null;
-			else v = ParseValue();
-			Consume(Lexemes.RightParenthesis);
-			return new Racr.AstNode(spec, "OrdinaryQuestion", n, l, t, v);
-		case "~?":
-			c = new Racr.AstNode(spec, "ComputedQuestion", ParseSymbol(), ParseString(), ParseExpression());
-			Consume(Lexemes.RightParenthesis);
-			return c;
-		case "~>":
-			n = ParseSymbol();
-			Consume(Lexemes.RightParenthesis);
-			return new Racr.AstNode(spec, "Use", n);
-		case "~!":
-			v = ParseValue();
-			Consume(Lexemes.RightParenthesis);
-			return new Racr.AstNode(spec, "Constant", v);
-		case "~~":
-			o = ParseIdentifier();
-			a = new List<Racr.AstNode>();
-			while (Lexeme == Lexemes.LeftParenthesis) a.Add(ParseExpression());
-			Consume(Lexemes.RightParenthesis);
-			return new Racr.AstNode(spec, "Computation", o, new Racr.AstList(a.ToArray()));
-		default: throw new Exception("Parse Exception");
-		}
-	}
-
-	public Racr.AstNode ParseAst() {
-		var ast = ParseExpression();
-		Consume(Lexemes.EOF);
-		return ast;
-	}
-}
-
 
 class Questionnaire {
 
-	static QL ql;
-
-	static void UpdateQuestions(Racr.AstNode n) {
+	static public void UpdateQuestions(Racr.AstNode n) {
 		switch (n.NodeType()) {
 		case "Form":
 		case "Group":
@@ -627,26 +447,17 @@ class Questionnaire {
 			break;
 		}
 	}
-
 	public static void Main(string[] args) {
 
-		string path;
-		/*
-		if (args.Length == 1) path = args[0];
-		else {
-			Console.Write("Enter name: ");
-			path = Console.ReadLine();
+		Racr.AstNode ast;
+		if (args.Length == 1) {
+			var parser = new Parser(QL.Ql, File.OpenText(args[0]).ReadToEnd());
+			ast = parser.ParseAst();
 		}
-		*/
-		path = "../../../../../foo.questionnaire";
-		path = "../../../../../correct-1.questionnaire";
+		else ast = new Racr.AstNode(QL.Ql, "Form", new Racr.AstList());
+		ast.Render();
+		UpdateQuestions(ast);
 
-		ql = new QL();
-		var parser = new Parser(ql, File.OpenText(path).ReadToEnd());
-		var form = parser.ParseAst();
-		form.Render();
-		UpdateQuestions(form);
-
-		Application.Run(form.Widget().Parent as Form);
+		Application.Run();
 	}
 }
