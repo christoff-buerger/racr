94efec46f4bb62c46afd71dee2c5dd8ce44ba40b
cbuerger <christoff.buerger@gmx.net>
2015-04-29 21:46:30 +0200

    Examples (ttc-2015-fuml-activity-diagrams): Added & tested input parser (all tests pass).

--- examples/ttc-2015-fuml-activity-diagrams/user-interface.scm
+++ examples/ttc-2015-fuml-activity-diagrams/user-interface.scm
@@ -7,8 +7,8 @@
 
 (library
  (ttc-2015-fuml-activity-diagrams user-interface)
- (export tparse)
+ (export parse)
  (import (rnrs) (racr testing) (ttc-2015-fuml-activity-diagrams parser))
  
- (define (tparse f)
-   (parse f)));(print-ast (tparse "examples/correct/test1.ad") (list) (current-output-port))
+ (define (parse f)
+   (parse-diagram f)));(print-ast (tparse "examples/correct/test1.ad") (list) (current-output-port))
