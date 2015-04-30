262456c442d489d7bc6e1a6bb6e6f17dcee21489
twobit <s8572327@gmail.com>
2015-04-30 14:31:40 +0200

    create adjusted racr lib via python script

---
+++ racr-net/racr/transcribe.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import re
+
+b = False
+
+for f in ["core", "testing"]:
+
+	in_path = "../../racr/" + f + ".scm"
+	out_path = f + ".sls"
+
+	in_file = file(in_path)
+	out_file = file(out_path, "w")
+
+	while 1:
+		l = in_file.readline()
+		if not l: break
+
+		l = re.sub(r"(hashtable-(ref|set!|delete!|contains\?|entries))(\s)", r"\1*\3", l)
+		l = re.sub(r"(\(import .*)(\))", r"\1 (racr hash)\2", l)
+
+		if f == "core":
+			l = re.sub(r"(\s*)\(export\w*", r"\g<0>\n\1 node-dot-net-instance\n\1 node-dot-net-instance-set!\n", l)
+			l = re.sub(r"(\s*)\(mutable annotations\)", r"\g<0>\n\1(mutable dot-net-instance)", l)
+
+
+			if not b and re.search(r"\(list\)\)\)\)\)\)", l):
+				b = True
+				l = re.sub(r"(\s*)\(list\)", r"\g<0>\n\1#f", l)
+
+		out_file.write(l)
