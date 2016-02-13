e34af44a16544f36027a91aca12631ad44f09da2
cbuerger <christoff.buerger@gmx.net>
2016-02-13 01:51:59 +0100

    Working on issue #54 (RACR-NET clean-up): The install-libraries.bash script now also compiles RACR-libraries to .NET assemblies (dlls) using IronScheme (related to issue #44).

--- install-libraries.bash
+++ install-libraries.bash
@@ -7,27 +7,18 @@
 
 ################################################################################################################ Parse arguments:
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-known_systems=( racket guile larceny )
-selected_systems=()
 known_libraries=( "$script_dir"/racr )
 known_libraries+=( $(find "$script_dir" -type f -name dependencies.txt | sed s/\\/dependencies.txt$// | grep -v /racr$) )
-selected_libraries=()
 
 while getopts s:i: opt
 do
 	case $opt in
 		s)
-			if [[ " ${known_systems[@]} " =~ " ${OPTARG} " ]]
+			"$script_dir/list-scheme-systems.bash" -s "$OPTARG"
+			if [ $? -eq 0 ]
 			then
-				if which "${OPTARG}" > /dev/null
-				then
-					selected_systems+=( "$OPTARG" )
-				else
-					echo " !!! ERROR: Scheme system [$OPTARG] not installed !!!" >&2
-					exit 2
-				fi
+				selected_systems+=( "$OPTARG" )
 			else
-				echo " !!! ERROR: Unknown [$OPTARG] Scheme system !!!" >&2
 				exit 2
 			fi;;
 		i)
@@ -46,34 +37,27 @@ do
 				exit 2
 			fi;;
 		?)
-			echo "Usage: -s Scheme system (${known_systems[@]})." >&2
+			echo "Usage: -s Scheme system (`"$script_dir/list-scheme-systems.bash" -i`)." >&2
 			echo "          Several Scheme systems can be set. If no system is selected," >&2
 			echo "          the libraries are installed for all available systems." >&2
 			echo "       -i RACR libraries to install." >&2
 			echo "          Several libraries can be set. If no library is selected," >&2
 			echo "          all libraries are installed." >&2
-			exit 2
+			exit 2;;
 	esac
 done
 shift $(( OPTIND - 1 ))
 
-if [ -z "$selected_systems" ]
+if [ -z ${selected_systems+x} ]
 then
-	for s in ${known_systems[@]}
-	do
-		if which "$s" > /dev/null
-		then
-			selected_systems+=( "$s" )
-		fi
-	done
-	if [ -z "$selected_systems" ]
+	selected_systems=`"$script_dir/list-scheme-systems.bash" -i`
+	if [ ! $? -eq 0 ]
 	then
-		echo " !!! ERROR: No Scheme system found !!!" >&2
 		exit 2
 	fi
 fi
 
-if [ -z "$selected_libraries" ]
+if [ -z ${selected_libraries+x} ]
 then	
 	selected_libraries=${known_libraries[@]}
 fi
@@ -87,7 +71,7 @@ then
 		configuration_to_parse="$l/dependencies.txt"
 		. "$script_dir/parse-configuration.bash" # Sourced script sets configuration!
 		if [[ " ${supported_systems[@]} " =~ "racket" ]]
-			then
+		then
 			rm -rf "$l/racket-bin"
 			mkdir -p "$l/racket-bin/`basename "$l"`"
 			lib_path=""
@@ -172,4 +156,68 @@ then
 	cd "$script_dir"
 	rm compile-stale
 	)
+fi
+
+if [[ " ${selected_systems[@]} " =~ "iron-scheme" ]]
+then
+	echo "=========================================>>> Compile for IronScheme:"
+	if [ ! -d "$script_dir/racr-net/ironscheme-bin" ] # One time setup: copy IronScheme distribution.
+	then
+		console=`which IronScheme.Console-v4.exe`
+		cp -r "`dirname "$console"`" "$script_dir/racr-net/ironscheme-bin/"
+	fi
+	for l in ${selected_libraries[@]}
+	do
+
+		library=`basename "$l"`
+		library_bin="$l/ironscheme-bin"
+		if [ "$library" == "racr" ] # Work around for the ``empty list is null and thus invalid key for hashtable'' bug.
+		then
+			mkdir "$script_dir/racr-net/racr"
+			# Use subshell for local directory changes via cd:
+			(
+			cd "$script_dir/racr-net"
+			"$script_dir/racr-net/transcribe.py"
+			)
+			rm -rf "$library_bin"
+			mkdir -p "$library_bin/$library"
+			cp -p "$script_dir/racr-net/hashtable-iron-scheme-adapter.sls" "$library_bin"
+			mv "$script_dir/racr-net/racr/"*.sls "$library_bin/$library"
+			rm -rf "$script_dir/racr-net/racr"
+			
+			rm -f "$script_dir/racr-net/ironscheme-bin/hashtable-iron-scheme-adapter.dll"
+			rm -f "$script_dir/racr-net/ironscheme-bin/racr.core.dll"
+			rm -f "$script_dir/racr-net/ironscheme-bin/racr.testing.dll"
+			echo "(import (hashtable-iron-scheme-adapter) (racr core) (racr testing))" > \
+				"$library_bin/compile-script.sls"
+			echo "(library-path (list \"$library_bin\")) (compile \"$library_bin/compile-script.sls\")" | \
+				mono "$script_dir/racr-net/ironscheme-bin/IronScheme.Console-v4.exe"
+			
+			continue
+		fi
+		configuration_to_parse="$l/dependencies.txt"
+		. "$script_dir/parse-configuration.bash" # Sourced script sets configuration!
+		if [[ " ${supported_systems[@]} " =~ "iron-scheme" ]]
+		then
+			rm -rf "$library_bin"
+			mkdir -p "$library_bin/$library"
+			lib_path="(list)"
+			for x in ${required_libraries[@]}
+			do
+				lib_path="(cons \"$x/ironscheme-bin\" $lib_path)"
+			done
+			lib_path="(library-path (cons \"$script_dir/racr-net/ironscheme-bin\" $lib_path))"
+			to_compile="(import "
+			for x in ${required_sources[@]}
+			do
+				source_file=`basename "$x"`
+				cp -p "$x.scm" "$library_bin/$library/$source_file.sls"
+				to_compile="$to_compile ($library $source_file)"
+				rm -f "$script_dir/racr-net/ironscheme-bin/$library.$source_file.dll"
+			done
+			echo "$to_compile)" > "$library_bin/compile-script.sls"
+			echo "$lib_path (compile \"$library_bin/compile-script.sls\")" | \
+				mono "$script_dir/racr-net/ironscheme-bin/IronScheme.Console-v4.exe"
+		fi
+	done
 fi
