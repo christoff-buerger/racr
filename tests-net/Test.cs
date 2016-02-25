7b9b12832b02591f9c1cc95eb56f9da495373667
cbuerger <christoff.buerger@gmx.net>
2016-02-25 03:25:55 +0100

    Working on issue #54 (RACR-NET cleanup): Finished cleanup of RACR-NET tests in tests-net.

--- tests-net/Test.cs
+++ tests-net/Test.cs
@@ -2,7 +2,7 @@
 	This program and the accompanying materials are made available under the
 	terms of the MIT license (X11 license) which accompanies this distribution.
 	
-	Author: D. Langner, C. Bürger
+	Author: C. Bürger, D. Langner
 */
 
 /*
@@ -20,60 +20,85 @@ using IronScheme;
 using IronScheme.Runtime;
 using NUnit.Framework;
 
-[TestFixture]
-public class RacrTests {
-	const string racrPath = "../";
-	static void EvalScript(string path) { File.ReadAllText(racrPath + "../../" + path).Eval(); }
-	Callable interpretCorrect;
-	Callable interpretIncorrect;
-
-	[SetUp]
-	public void Init() {
-		"(import (racr core) (racr testing))".Eval();
-
-		// extend library path
-		"(library-path (cons {0} (library-path)))".Eval(racrPath + "examples");
-
-		// siple
-		"(import (siple main) (siple exception-api))".Eval();
-		interpretCorrect = "siple-interpret".Eval<Callable>();
-		interpretIncorrect = @"
-		(lambda (x)
-		  (assert-exception
-		    siple-exception?
-			(siple-interpret x)))
-		".Eval<Callable>();
+[TestFixture] public class RacrTests {
+	public RacrTests() {
+		foreach (var libpath in new String[] {
+			"racr",
+			"racr-meta",
+			"examples/atomic-petrinets",
+			"examples/composed-petrinets",
+			"examples/ttc-2015-fuml-activity-diagrams",
+			"examples/siple",
+		}) { ("(library-path (cons \"../../../" + libpath + "/ironscheme-bin\" (library-path)))").Eval(); }
 	}
 
-	[Test] public void SchemeAdd() 				{ Assert.AreEqual(3, "(+ 1 2)".Eval<int>()); }
-
-	[Test] public void RacrTestAstConstruction()		{ EvalScript("tests/ast-construction.scm"); }
-	[Test] public void RacrTestAttributeEvaluationBasics()	{ EvalScript("tests/attribute-evaluation-basics.scm"); }
-	[Test] public void RacrTestContinuationsInEquations()	{ EvalScript("tests/continuations-in-equations.scm"); }
-	[Test] public void RacrTestPatterns()			{ EvalScript("tests/patterns.scm"); }
-	[Test] public void RacrTestRewriteBasics()		{ EvalScript("tests/rewrite-basics.scm"); }
-	[Test] public void RacrTestRewriteBuds()		{ EvalScript("tests/rewrite-buds.scm"); }
-	[Test] public void RacrTestRewriteLists()		{ EvalScript("tests/rewrite-lists.scm"); }
-	[Test] public void RacrTestRewriteRefineAbstract()	{ EvalScript("tests/rewrite-refine-abstract.scm"); }
-	[Test] public void RacrTestRewriteStrategies()		{ EvalScript("tests/rewrite-strategies.scm"); }
-
-	[Test]
-	public void RacrExamples() {
-		foreach (var test in new string[] {
-			"binary-numbers/binary-numbers.scm",
-			"petrinets/examples/cookie-automaton.scm",
-			"petrinets/examples/purchase-processing.scm",
-			"petrinets/examples/runtime-structure-example-slide.scm",
-			"state-machines/state-machines.scm",
-		}) {
-			var path = "examples/" + test;
-			Console.WriteLine("loading {0} ...", path);
-			EvalScript(path);
-		}
+	[Test] public void SchemeAvailable() {
+		Assert.AreEqual(3, "(+ 1 2)".Eval<int>());
 	}
 
-	[Test]
-	public void SipleCorrectExamples() {
+	[Test] public void Tests() {
+		foreach (var test in new String[] {
+			"ast-construction",
+			"attribute-evaluation-basics",
+			"continuations-in-equations",
+			"patterns",
+			"rewrite-basics",
+			"rewrite-buds",
+			"rewrite-lists",
+			"rewrite-refine-abstract",
+			"rewrite-strategies",
+		}) { File.ReadAllText("../../../tests/" + test + ".scm").Eval(); }
+	}
+
+	[Test] public void BinaryNumbers() {
+		File.ReadAllText("../../../examples/binary-numbers/binary-numbers.scm").Eval();
+	}
+
+	[Test] public void StateMachines() {
+		File.ReadAllText("../../../examples/state-machines/state-machines.scm").Eval();
+	}
+
+	[Test] public void AtomicPetrinets() {
+		foreach (var test in new String[] {
+			"cookie-automaton",
+			"syntax-tests",
+		}) { File.ReadAllText("../../../examples/atomic-petrinets/examples/" + test + ".scm").Eval(); }
+	}
+
+	[Test] public void ComposedPetrinets() {
+		foreach (var test in new String[] {
+			"purchase-processing",
+			"runtime-structure-example-slide",
+			"syntax-tests",
+		}) { File.ReadAllText("../../../examples/composed-petrinets/examples/" + test + ".scm").Eval(); }
+	}
+
+	[Test] public void ActivityDiagrams() {
+		const String path = "../../../examples/ttc-2015-fuml-activity-diagrams/examples/contest-tests/";
+		"(import (rnrs) (ttc-2015-fuml-activity-diagrams user-interface))".Eval();
+		Callable interpretWithoutInput = "(lambda (d) (run-activity-diagram d #f 6 #f))".Eval<Callable>();
+		Callable interpretWithInput = "(lambda (d i) (run-activity-diagram d i 6 #f))".Eval<Callable>();
+		foreach (var diagram in new string[] {
+			"test1.ad",
+			"test2.ad",
+			"test3.ad",
+			"test4.ad",
+			"testperformance_variant1.ad",
+			"testperformance_variant2.ad",
+			"testperformance_variant3_1.ad",
+		}) { interpretWithoutInput.Call(path + diagram); }
+		foreach (var diagram in new string[] {
+			"test6_false.ad",
+			"test6_true.ad",
+			"testperformance_variant3_2.ad",
+		}) { interpretWithInput.Call(path + diagram, path + diagram + "input"); }
+	}
+
+	[Test] public void Siple() {
+		const String path = "../../../examples/siple/examples/";
+		"(import (rnrs) (racr testing) (siple main) (siple exception-api))".Eval();
+		Callable interpretCorrect = "siple-interpret".Eval<Callable>();
+		Callable interpretIncorrect = "(lambda (p) (assert-exception siple-exception? (siple-interpret p)))".Eval<Callable>();
 		foreach (var test in new string[] {
 			"abnormal_termination.siple",
 			"assertions.siple",
@@ -87,15 +112,7 @@ public class RacrTests {
 			"relational_arithmetics.siple",
 			"scopes.siple",
 			"type_coercions.siple",
-		}) {
-			var path = "examples/siple/examples/correct/" + test;
-			Console.WriteLine("interpreting {0} ...", path);
-			interpretCorrect.Call(path);
-		}
-	}
-
-	[Test]
-	public void SipleIncorrectExamples() {
+		}) { interpretCorrect.Call(path + "correct/" + test); }
 		foreach (var test in new string[] {
 			"abnormal_termination.siple",
 			"assertions.siple",
@@ -105,82 +122,97 @@ public class RacrTests {
 			"procedure_basics.siple",
 			"relational_arithmetics.siple",
 			"scopes.siple",
-		}) {
-			var path = "examples/siple/examples/incorrect/" + test;
-			Console.WriteLine("interpreting {0} ...", path);
-			interpretIncorrect.Call(path);
-		}
+		}) { interpretIncorrect.Call(path + "incorrect/" + test); }
 	}
 }
 
-[TestFixture]
-public class App {
-	[Test]
-	public void TestRewriteRefine() {
-		var spec = new Racr.Specification();
+[TestFixture] public class RewritesTests {
+	private Racr.Specification spec = null;
 
+	public RewritesTests() {
+		spec = new Racr.Specification();
 		spec.AstRule("S->A");
-		spec.AstRule("A->a");
-		spec.AstRule("Aa:A->b-c");
+		spec.AstRule("A->a-B<B1");
+		spec.AstRule("Aa:A->B<B2-c");
+		spec.AstRule("B->");
 		spec.CompileAstSpecifications("S");
 		spec.CompileAgSpecifications();
-
-		var ast = new Racr.AstNode(spec, "S", new Racr.AstNode(spec, "A", 1));
-		var A = ast.Child("A");
-
-		Console.WriteLine("{0}", A.NumChildren() == 1);
-		Console.WriteLine("{0}", A.NodeType() == "A");
-
-		A.RewriteRefine("Aa", 2, 3);
-
-		Console.WriteLine("{0}", A.NumChildren() == 3);
-		Console.WriteLine("{0}", A.NodeType() == "Aa");
-
-		foreach (var c in A.Children()) Console.WriteLine(c);
 	}
 
-	[Test]
-	public void TestRewriteAbstract() {
-		var spec = new Racr.Specification();
-
-		spec.AstRule("S->A");
-		spec.AstRule("A->a");
-		spec.AstRule("Aa:A->b-c");
-		spec.CompileAstSpecifications("S");
-		spec.CompileAgSpecifications();
-
-		var ast = new Racr.AstNode(spec, "S", new Racr.AstNode(spec, "Aa", 1, 2, 3));
+	[Test] public void TestRewriteRefine() {
+		var ast = new Racr.AstNode(spec, "S", new Racr.AstNode(spec, "A", 1, new Racr.AstNode(spec, "B")));
 		var A = ast.Child("A");
+		var B1 = A.Child("B1");
 
-		Console.WriteLine(A.NumChildren() == 3);
-		Console.WriteLine(A.NodeType() == "Aa");
+		Assert.IsTrue(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 2);
+		Assert.AreEqual(A.NodeType(), "A");
+		Assert.AreEqual(A.Child<int>(1), 1);
+		Assert.AreEqual(A.Child(2), B1);
+
+		A.RewriteRefine("Aa", new Racr.AstNode(spec, "B"), 4);
+
+		Assert.IsTrue(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 4);
+		Assert.AreEqual(A.NodeType(), "Aa");
+		Assert.AreEqual(A.Child<int>(1), 1);
+		Assert.AreEqual(A.Child(2), B1);
+		Assert.AreEqual(A.Child(3).NodeType(), "B");
+		Assert.AreEqual(A.Child<int>(4), 4);
+	}
+
+	[Test] public void TestRewriteAbstract() {
+		var ast = new Racr.AstNode(spec, "S", new Racr.AstNode(spec, "Aa", 1, new Racr.AstNode(spec, "B"), new Racr.AstNode(spec, "B"), 4));
+		var A = ast.Child("A");
+		var B1 = A.Child("B1");
+		var B2 = A.Child("B2");
+
+		Assert.IsTrue(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 4);
+		Assert.AreEqual(A.NodeType(), "Aa");
+		Assert.AreEqual(A.Child<int>(1), 1);
+		Assert.AreEqual(A.Child(2), B1);
+		Assert.AreEqual(A.Child(3), B2);
+		Assert.AreEqual(A.Child<int>(4), 4);
 
 		var c = A.RewriteAbstract("A");
-		foreach (var x in c) Console.WriteLine(x);
+		Assert.AreEqual(c.Length, 2);
+		Assert.AreEqual(c[0], B2);
+		Assert.AreEqual(c[1], 4);
 
-		Console.WriteLine(A.NumChildren() == 1);
-		Console.WriteLine(A.NodeType() == "A");
+		Assert.IsTrue(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 2);
+		Assert.AreEqual(A.NodeType(), "A");
+		Assert.AreEqual(A.Child<int>(1), 1);
+		Assert.AreEqual(A.Child(2), B1);
 	}
 
-	[Test]
-	public void TestRewriteSubtree() {
-		var spec = new Racr.Specification();
-
-		spec.AstRule("S->A");
-		spec.AstRule("A->a");
-		spec.AstRule("Aa:A->b-c");
-		spec.CompileAstSpecifications("S");
-		spec.CompileAgSpecifications();
-
-		var ast = new Racr.AstNode(spec, "S", new Racr.AstNode(spec, "A", 42));
+	[Test] public void TestRewriteSubtree() {
+		var ast = new Racr.AstNode(spec, "S", new Racr.AstNode(spec, "A", 42, new Racr.AstNode(spec, "B")));
 		var A = ast.Child("A");
+		var B1 = A.Child("B1");
 
-		Console.WriteLine(A.HasParent());
-		Console.WriteLine(ast.Child("A").NodeType());
+		Assert.IsTrue(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 2);
+		Assert.AreEqual(A.NodeType(), "A");
+		Assert.AreEqual(A.Child<int>(1), 42);
+		Assert.AreEqual(A.Child(2), B1);
 
-		A.RewriteSubtree(new Racr.AstNode(spec, "Aa", 1, 2, 3));
+		A.RewriteSubtree(new Racr.AstNode(spec, "Aa", 1, new Racr.AstNode(spec, "B"), new Racr.AstNode(spec, "B"), 42));
+		Assert.IsFalse(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 2);
+		Assert.AreEqual(A.NodeType(), "A");
+		Assert.AreEqual(A.Child<int>(1), 42);
+		Assert.AreEqual(A.Child(2), B1);
 
-		Console.WriteLine(A.HasParent());
-		Console.WriteLine(ast.Child("A").NodeType());
+		A = ast.Child("A");
+		Assert.IsTrue(A.HasParent());
+		Assert.AreEqual(A.NumChildren(), 4);
+		Assert.AreEqual(A.NodeType(), "Aa");
+		Assert.AreEqual(A.Child<int>(1), 1);
+		Assert.AreNotEqual(A.Child(2), B1);
+		Assert.AreEqual(A.Child(2).NodeType(), "B");
+		Assert.AreEqual(A.Child(3).NodeType(), "B");
+		Assert.AreEqual(A.Child<int>(4), 42);
 	}
 }
