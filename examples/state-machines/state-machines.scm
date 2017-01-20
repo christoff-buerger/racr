34f712befaa9732a765647925b8c4e370ecf5e53
cbuerger <christoff.buerger@gmx.net>
2017-01-21 00:17:41 +0100

    Fixed issue #72 (state machines example: shifting semantic tests from macros to well-formedness attributes).

--- examples/state-machines/state-machines.scm
+++ examples/state-machines/state-machines.scm
@@ -13,35 +13,45 @@
 (define (->State* n)              (ast-child 'State* n))
 (define (->Transition* n)         (ast-child 'Transition* n))
 (define (->initial n)             (ast-child 'initial n))
-(define (->finals n)              (ast-child 'finals n))
 (define (->name n)                (ast-child 'name n))
+(define (->final n)               (ast-child 'final n))
 (define (->source n)              (ast-child 'source n))
 (define (->target n)              (ast-child 'target n))
 (define (->* n)                   (ast-children n))
-(define (<- n)                    (ast-parent n))
 
 ; Attribute Accessors:
-(define (=final? n)               (att-value 'final? n))
+(define (=transitions n)          (att-value 'transitions n))
 (define (=lookup-state n s)       (att-value 'lookup-state n s))
 (define (=initial-state n)        (att-value 'initial-state n))
-(define (=filter-transitions n f) (att-value 'filter-transitions n f))
 (define (=successors n)           (att-value 'successors n))
 (define (=reachable n)            (att-value 'reachable n))
 (define (=correct? n)             (att-value 'correct? n))
 
 ; AST Constructors:
-(define (:StateMachine s t i f)
-  (create-ast sm-spec 'StateMachine
-              (list (create-ast-list s) (create-ast-list t) i f)))
-(define (:State n)
-  (create-ast sm-spec 'State (list n)))
+(define (:StateMachine s t i)
+  (create-ast
+   sm-spec
+   'StateMachine
+   (list (create-ast-list s) (create-ast-list t) i)))
+(define (:State n f)
+  (create-ast sm-spec 'State (list n f)))
 (define (:Transition s t)
   (create-ast sm-spec 'Transition (list s t)))
 
 ; Support functions:
+(define (make-set l)
+  (let loop ((input l) (output (list)))
+    (cond
+      ((null? input)
+       output)
+      ((memq (car input) output)
+       (loop (cdr input) output))
+      (else (loop (cdr input) (cons (car input) output))))))
 (define (set-union s1 s2)
   (append (filter (lambda (e1) (not (memq e1 s2))) s1) s2))
-(define (set-eq s1 s2)
+(define (set-difference s1 s2)
+  (remp (lambda (e) (memq e s2)) s1))
+(define (set-eq? s1 s2)
   (and (= (length s1) (length s2)) (for-all (lambda (e) (memq e s2)) s1)))
 
 (with-specification
@@ -49,8 +59,8 @@
  
  ;;; AST Scheme:
  
- (ast-rule 'StateMachine->State*-Transition*-initial-finals)
- (ast-rule 'State->name)
+ (ast-rule 'StateMachine->State*-Transition*-initial)
+ (ast-rule 'State->name-final)
  (ast-rule 'Transition->source-target)
  
  (compile-ast-specifications 'StateMachine)
@@ -58,13 +68,10 @@
  ;;; Query Support:
  
  (ag-rule
-  final? ; Return, whether a state is a final state or not.
-  ((StateMachine State*)
+  transitions ; List of transitions of state machine.
+  (StateMachine
    (lambda (n)
-     (define name (->name n))
-     (find
-      (lambda (final) (eq? name final))
-      (->finals (<- (<- n)))))))
+     (->* (->Transition* n)))))
  
  ;;; Name Analysis:
  
@@ -82,13 +89,6 @@
    (lambda (n)
      (=lookup-state n (->initial n)))))
  
- (ag-rule
-  filter-transitions ; Return all transitions satisfying a given filter.
-  (StateMachine
-   #f
-   (lambda (n f)
-     (filter f (->* (->Transition* n))))))
- 
  ;;; Reachability:
  
  (ag-rule
@@ -98,7 +98,9 @@
      (define name (->name n))
      (map
       (lambda (n) (=lookup-state n (->target n)))
-      (=filter-transitions n (lambda (n) (eq? (->source n) name)))))))
+      (filter
+       (lambda (n) (eq? (->source n) name))
+       (=transitions n))))))
  
  (ag-rule
   reachable ; Compute all states reachable from a state.
@@ -117,20 +119,39 @@
  (ag-rule
   correct? ; Check well-formedness semantics.
   
-  (StateMachine ; The state machine is correct, iff all states are correct.
-   (lambda (n)
-     (not
-      (ast-find-child
-       (lambda (i n) (not (=correct? n)))
-       (->State* n)))))
-  
-  (State
+  (StateMachine ; The state machine is correct, if...
    (lambda (n)
      (and
-      (or ; From every state, except finals, a final must be reachable.
-       (=final? n)
-       (find =final? (=reachable n)))
-      (or ; All states, except the initial, must be reachable from the initial.
+      (=initial-state n) ; ...its inital state is defined and...
+      (not ; ...all its states and...
+       (ast-find-child
+        (lambda (i n) (not (=correct? n)))
+        (->State* n)))
+      (not ; ...transitions are correct.
+       (ast-find-child
+        (lambda (i n) (not (=correct? n)))
+        (->Transition* n))))))
+
+  (Transition ; A transition is correct, if...
+   (lambda (n)
+     (define source (->source n))
+     (define target (->target n))
+     (and
+      (symbol? source) ; ...its source and...
+      (symbol? target) ; ...target are symbols...
+      (=lookup-state n source) ; ...refering to respectively named states.
+      (=lookup-state n target))))
+  
+  (State ; A state is correct, if...
+   (lambda (n)
+     (define name (->name n))
+     (and
+      (symbol? name) ; ...its name is a symbol and...
+      (eq? (=lookup-state n name) n) ; ...unique and...
+      (or ; ...from every state, except finals, a final is reachable and...
+       (->final n)
+       (find ->final (=reachable n)))
+      (or ; ...all states, except the initial, are reachable from the initial.
        (eq? n (=initial-state n))
        (memq n (=reachable (=initial-state n))))))))
  
@@ -139,35 +160,16 @@
 ;;; Syntax:
 
 (define-syntax state-machine:
-  (lambda (x)
-    (define identifier-list?
-      (lambda (l)
-        (for-all identifier? l)))
-    (syntax-case x (->)
-      ((_ initial (final ...) (source -> target) ...)
-       (and
-        (identifier? #'initial)
-        (identifier-list? #'(final ...))
-        (identifier-list? #'(source ...))
-        (identifier-list? #'(target ...))
-        (not (null? #'(final ...))))
-       (with-syntax
-           (((state ...)
-             (let loop ((input #'(initial final ... source ... target ...))
-                        (output (list)))
-               (if (null? input)
-                   output
-                   (if (find
-                        (lambda (o)
-                          (eq? (syntax->datum (car input)) (syntax->datum o)))
-                        output)
-                       (loop (cdr input) output)
-                       (loop (cdr input) (cons (car input) output)))))))
-         #`(:StateMachine
-            (list (:State 'state) ...)
-            (list (:Transition 'source 'target) ...)
-            'initial
-            (list 'final ...)))))))
+  (syntax-rules (->)
+    ((_ initial (final ...) (source -> target) ...)
+     (let* ((f (make-set (list 'final ...)))
+            (s (set-difference (make-set (list 'source ... 'target ...)) f)))
+       (:StateMachine
+        (append
+         (map (lambda (s) (:State s #t)) f)
+         (map (lambda (s) (:State s #f)) s))
+        (list (:Transition 'source 'target) ...)
+        'initial)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                            Examples and tests                           ;;;
@@ -175,9 +177,9 @@
 
 (define (run-tests)
   (define (assert-sucessors sm s l)
-    (assert (set-eq (map ->name (=successors (=lookup-state sm s))) l)))
+    (assert (set-eq? (map ->name (=successors (=lookup-state sm s))) l)))
   (define (assert-reachable sm s l)
-    (assert (set-eq (map ->name (=reachable (=lookup-state sm s))) l)))
+    (assert (set-eq? (map ->name (=reachable (=lookup-state sm s))) l)))
   
   (define sm-1
     ; Correct machine:
@@ -263,12 +265,12 @@
   (assert-sucessors sm-1 's1 '(s1 s2 s3))
   (assert-reachable sm-1 's6 '(s1 s2 s3 s4 s5 s6 s7))
   (assert (=correct? sm-1))
-  (rewrite-add (->State* sm-1) (:State 'sn))
+  (rewrite-add (->State* sm-1) (:State 'sn #f))
   (rewrite-add (->Transition* sm-1) (:Transition 's3 'sn))
   (assert (not (=correct? sm-1)))
-  (rewrite-terminal 'finals sm-1 (cons 'sn (->finals sm-1)))
+  (rewrite-terminal 'final (=lookup-state sm-1 'sn) #t)
   (assert (=correct? sm-1))
-  (rewrite-terminal 'finals sm-1 (list 's4))
+  (rewrite-terminal 'final (=lookup-state sm-1 'sn) #f)
   (assert (not (=correct? sm-1)))
   (rewrite-add (->Transition* sm-1) (:Transition 'sn 's5))
   (assert (=correct? sm-1))
