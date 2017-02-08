7d32d076571d6e78563e8a927d83662f445aedbc
rschoene <rene.schoene@tu-dresden.de>
2017-02-08 10:19:21 +0100

    Silence which.

--- list-scheme-systems.bash
+++ list-scheme-systems.bash
@@ -28,13 +28,13 @@ do
 			found=""
 			for s in guile racket larceny petite sagittarius
 			do
-				if which "$s" > /dev/null
+				if which "$s" &> /dev/null
 				then
 					echo "$s"
 					found=true
 				fi
 			done
-			if which IronScheme.Console-v4.exe > /dev/null
+			if which IronScheme.Console-v4.exe &> /dev/null
 			then
 				echo ironscheme
 				found=true
