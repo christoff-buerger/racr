0d3230a1f9ebcdb76e1565944328fd4a666d2f10
cbuerger <christoff.buerger@gmx.net>
2017-10-21 00:07:55 +0200

    Fixed issue #83 (test-run summary and continue-on-error flag for test script): Added new -x flag to the 'tests/run.bash' script that deactivates failing on first error and gives a summary at the end.

--- tests/run.bash
+++ tests/run.bash
@@ -10,17 +10,21 @@ set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ################################################################################################################ Parse arguments:
-while getopts s:h opt
+while getopts s:xh opt
 do
 	case $opt in
 		s)
 			"$script_dir/../deploying/deployment-scripts/list-scheme-systems.bash" -s "$OPTARG"
 			selected_systems+=( "$OPTARG" );;
+		x)
+			abort_on_failed_test="false";;
 		h|?)
 			echo "Usage: -s Scheme system (optional multi-parameter). Permitted values:" >&2
 			echo "`"$script_dir/../deploying/deployment-scripts/list-scheme-systems.bash" -i | \
 				sed 's/^/             /'`" >&2
 			echo "          If no system is selected, all installed and officially supported systems are tested." >&2
+			echo "       -x Do not abort testing on error (multi-flag)." >&2
+			echo "          By default, testing is aborted as soon as any test failed." >&2
 			exit 2;;
 	esac
 done
@@ -37,7 +41,17 @@ then
 	selected_systems=`"$script_dir/../deploying/deployment-scripts/list-scheme-systems.bash" -i`
 fi
 
+if [ -z ${abort_on_failed_test+x} ]
+then
+	abort_on_failed_test="true"
+fi
+
 ###################################################################################################### Define execution function:
+tests_executed=0
+tests_passed=0
+tests_failed=0
+tests_skipped=0
+
 run(){
 	program="$1"
 	library="$2"
@@ -59,17 +73,27 @@ run(){
 		error_status=$?
 		set -e
 		set -o pipefail
+		tests_executed=$(( tests_executed + 1 ))
 		if [ $error_status -eq 0 -a ! -z "$error_message" ]
 		then
 			error_status=1
 		fi
 		case $error_status in
-			0) printf " $s";;	# all correct
-			2) printf " -$s-";;	# configuration error for Scheme system => test skipped
-			*)
-				echo " !$s!"	# test failed (execution error) => print error and abort testing
-				echo "$error_message"
-				exit $error_status;;
+			0) # all correct => test passed
+				printf " $s"
+				tests_passed=$(( tests_passed + 1));;
+			2) # configuration error for Scheme system => test skipped
+				printf " -$s-"
+				tests_skipped=$(( tests_skipped + 1));;
+			*) # test failed (execution error) => print error and...
+				echo " !$s!"
+				echo "$error_message" >&2
+				if [[ "$abort_on_failed_test" =~ "true" ]] # ...abort testing if requested
+				then
+					echo " !!! ERROR: Testing aborted because of failed test !!!" >&2
+					exit $error_status
+				fi
+				tests_failed=$(( tests_failed + 1 ));;
 		esac
 	done
 	echo ""
@@ -121,3 +145,15 @@ for f in "$script_dir"/../examples/siple/examples/incorrect/*.siple
 do
 	run "$script_dir/../examples/siple/run.scm" "" "$f" ":true:"
 done
+
+status_message="=====T=E=S=T===S=U=M=M=A=R=Y=====
+Number of tests: $tests_executed
+Tests passed:    $tests_passed
+Tests skipped:   $tests_skipped
+Tests failed:    $tests_failed"
+if [ $tests_failed -gt 0 ]
+then
+	printf "%s\n" "$status_message" >&2
+	exit 2
+fi
+printf "%s\n" "$status_message"
