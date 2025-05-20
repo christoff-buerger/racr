0c389e2be78aef519c7e6a10c0b31ba611492743
Christoff Bürger <Christoff.Buerger@gmail.com>
2025-05-21 00:02:05 +0200

    #92: Incorporated ShellCheck proposals.

--- profiling/profiling-scripts/measure.bash
+++ profiling/profiling-scripts/measure.bash
@@ -17,7 +17,7 @@ unset failsave
 arguments="$* --"
 arguments="${arguments#*--}"
 
-if [ $# -eq 0 ]
+if (( $# == 0 ))
 then
 	"$script_dir/measure.bash" -h
 	exit $?
@@ -27,7 +27,7 @@ while getopts c:t:p:s:xh opt
 do
 	case $opt in
 		c)
-			if [ "${profiling_configuration+x}" = "" ]
+			if [[ ! -v "profiling_configuration" ]]
 			then
 				profiling_configuration="$OPTARG"
 			else
@@ -36,7 +36,7 @@ do
 			fi
 			;;
 		t)
-			if [ "${measurements_table+x}" = "" ]
+			if [[ ! -v "measurements_table" ]]
 			then
 				measurements_table="$OPTARG"
 			else
@@ -54,7 +54,7 @@ do
 			fi
 			;;
 		s)
-			if [ "${rerun_script+x}" = "" ]
+			if [[ ! -v "rerun_script" ]]
 			then
 				rerun_script="$OPTARG"
 			else
@@ -65,7 +65,7 @@ do
 		x)
 			failsave="-x"
 			;;
-		h|?)
+		h|*)
 			echo "Usage: -c Profiling configuration (mandatory parameter)." >&2
 			echo "       -t Measurements table used for recording (mandatory parameter)." >&2
 			echo "          Created if not existent. New measurements are appended." >&2
@@ -89,22 +89,22 @@ shift $(( OPTIND - 1 ))
 
 fixed_arguments=( "$@" )
 
-if [ -t 0 ] && [ $# -ge 1 ] && [ " $* --" != "$arguments" ]
+if [[ -t 0 ]] && (( $# != 0 )) && [[ " $* --" != "$arguments" ]]
 then
 	echo " !!! ERROR: Unknown [$*] command line arguments !!!" >&2
 	exit 2
 fi
 
-if [ "$to_profile" = "" ] || [ ! -x "$to_profile" ]
+if [[ "$to_profile" == "" ]] || [[ ! -x "$to_profile" ]]
 then
 	echo " !!! ERROR: No, non-existent or non-executable program to profile specified via -p parameter !!!" >&2
 	exit 64
 fi
 
-if [ "${rerun_script+x}" = "" ]
+if [[ ! -v "rerun_script" ]]
 then
 	rerun_script="/dev/null"
-elif [ "$rerun_script" = "" ] || { [ ! "$rerun_script" -ef "/dev/null" ] && [ -e "$rerun_script" ]; }
+elif [[ "$rerun_script" == "" ]] || { [[ ! "$rerun_script" -ef "/dev/null" ]] && [[ -e "$rerun_script" ]]; }
 then
 	echo " !!! ERROR: Invalid rerun script specified via -s parameter !!!" >&2
 	exit 2
@@ -119,16 +119,16 @@ my_exit(){
 	# Capture exit status (i.e., script success or failure):
 	exit_status=$?
 	# Close the recording pipe and wait until all measurements are recorded:
-	if [ "$recording_pid" != "" ]
+	if [[ "$recording_pid" != "" ]]
 	then
 		exec 3>&-
-		while s=$( ps -p "$recording_pid" -o state= ) && [[ "$s" && "$s" != 'Z' ]] 
+		while s=$( ps -p "$recording_pid" -o state= ) && [[ "$s" != "" && "$s" != 'Z' ]] 
 		do
 			sleep 1
 		done
 	fi
 	# Delete the rerun script in case a user interactively specified invalid measurement-parameters while generating it:
-	if [ -t 0 ] && [ "$exit_status" -gt 0 ] && [ "$valid_parameters" -eq 0 ] && [ ! "$rerun_script" -ef "/dev/null" ]
+	if [[ -t 0 ]] && (( exit_status > 0 )) && (( valid_parameters == 0 )) && [[ ! "$rerun_script" -ef "/dev/null" ]]
 	then
 		rm -f "$rerun_script"
 	fi
@@ -147,7 +147,7 @@ mkfifo "$measurements_pipe"
 recording_pid=$( "$script_dir/record.bash" -c "$profiling_configuration" -t "$measurements_table" -p "$measurements_pipe" )
 exec 3> "$measurements_pipe"
 
-if [ ! "$rerun_script" -ef "/dev/null" ]
+if [[ ! "$rerun_script" -ef "/dev/null" ]]
 then
 	mkdir -p "$( dirname "$rerun_script" )"
 	touch "$rerun_script"
@@ -196,7 +196,7 @@ parameter_adjustments=( "DUMMY DATE ADJUSTMENT" )
 for (( i = 1; i < number_of_parameters; i++ ))
 do
 	IFS='' read -r -p "${parameter_descriptions[$i]} [${parameter_names[$i]}]: " choice
-	if [ ! -t 0 ]
+	if [[ ! -t 0 ]]
 	then
 		echo "${parameter_descriptions[$i]} [${parameter_names[$i]}]: $choice"
 	fi
@@ -206,7 +206,7 @@ do
 	case "$choice" in
 		''|*[0-9]*)
 			IFS='' read -r -n1 -p "	Iterate? (y/n): " choice
-			if [ ! -t 0 ]
+			if [[ ! -t 0 ]]
 			then
 				printf "	Iterate? (y/n): %s" "$choice"
 			fi
@@ -220,14 +220,14 @@ do
 	case "$choice" in
 		[y]*)
 			IFS='' read -r -p "	Number of iterations (altogether): " choice
-			if [ ! -t 0 ]
+			if [[ ! -t 0 ]]
 			then
 				echo "	Number of iterations (altogether): $choice"
 			fi
 			echo "$choice" >> "$rerun_script"
 			case "$choice" in
 				''|*[0-9]*)
-					if [ "$choice" -lt 1 ]
+					if (( choice < 1 ))
 					then
 						echo " !!! ERROR: Invalid choice !!!" >&2
 						exit 2
@@ -240,7 +240,7 @@ do
 			esac
 			parameter_iterations+=( "$choice" )
 			IFS='' read -r -p "	Adjustment each iteration (excluding first): " choice
-			if [ ! -t 0 ]
+			if [[ ! -t 0 ]]
 			then
 				echo "	Adjustment each iteration (excluding first): $choice"
 			fi
@@ -277,7 +277,7 @@ current_parameter_iterations=( "DUMMY DATE ITERATION" )
 current_parameter=1
 undo=false
 
-while [ "$current_parameter" -ge 1 ]
+while (( current_parameter >= 1 ))
 do
 	if (( current_parameter >= number_of_parameters ))
 	then # perform measurement
@@ -314,7 +314,7 @@ do
 		print_color="\033[0;32m"
 		print_stderr_message="false"
 		print_results="false"
-		if [ -s "$measurements_stderr" ]
+		if [[ -s "$measurements_stderr" ]]
 		then
 			print_color="\033[1;33m"
 			print_stderr_message="true"
@@ -339,7 +339,7 @@ do
 		then
 			printf "%b" "$print_color" >&2
 			printf "  Measurement %s with exit code [%i] and " "$measurement_status" "$measurement_error" >&2
-			if [ -s "$measurements_stderr" ]
+			if [[ -s "$measurements_stderr" ]]
 			then
 				echo "error message (stderr):" >&2
 				# Command substitution removes trailing newlines from compacted message prefixed with ' >>':
@@ -375,7 +375,7 @@ do
 				done
 			fi
 		fi
-	elif [ "$undo" = true ] && \
+	elif [[ "$undo" = true ]] && \
 		(( current_parameter_iterations[current_parameter] < parameter_iterations[current_parameter] ))
 	then # redo with adjusted parameters
 		undo=false
@@ -383,7 +383,7 @@ do
 			current_parameter_values[current_parameter] + parameter_adjustments[current_parameter] ))
 		current_parameter_iterations[current_parameter]=$(( current_parameter_iterations[current_parameter] + 1 ))
 		current_parameter=$(( current_parameter + 1 ))
-	elif [ "$undo" = true ]
+	elif [[ "$undo" = true ]]
 	then # reinitialise and further backtrack
 		current_parameter_values[current_parameter]=${parameter_values[$current_parameter]}
 		current_parameter_iterations[current_parameter]=1
